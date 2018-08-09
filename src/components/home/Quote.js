382d19f87bbd6866cbaf2dc1c664518bb16058df
Guido van de Riet <guidovdriet@gmail.com>
2018-08-09 15:34:17 +0200

    Implement show/hide of cms data

---
+++ src/components/home/Quote.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Quote extends Component {
+  render() {
+    return(
+      <h2>Quote</h2>
+    )
+  }
+}
+
+export default Quote;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const indexPage = () => <h2>Home</h2>;
+import Quote from '../../src/components/home/Quote';
+import News from '../../src/components/home/News';
+
+class indexPage extends Component {
+  constructor(props) {
+    super();
+    const { acf } = props.data.allWordpressPage.edges[0].node;
+    this.state = {
+      ...acf,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.news && <Quote />}
+        {this.state.quote && <News />}
+      </div>
+    );
+  }
+}
 
 export default indexPage;
+
+export const query = graphql`
+  query getHomeData {
+    allWordpressPage(filter: { slug: { eq: "home" } }) {
+      edges {
+        node {
+          acf {
+            quote
+            news
+          }
+        }
+      }
+    }
+  }
+`;
